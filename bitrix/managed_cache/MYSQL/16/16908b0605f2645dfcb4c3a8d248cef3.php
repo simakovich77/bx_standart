d1ba8805c0ed2d0d6736c9f214a006ddad7962a3
sim.i.a <simakovich77@gmail.com>
2016-12-26 16:27:36 +0300

    sddddddd

--- bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
+++ bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001482757300';
-$dateexpire = '001482760900';
+$datecreate = '001482758772';
+$dateexpire = '001482762372';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:1;}';
 return true;
 ?>
