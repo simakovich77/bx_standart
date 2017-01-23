485be639918ead038bddbc419852a80c1690a286
simakovich77 <simakovich77@gmail.com>
2017-01-23 04:56:07 +0300

    sdfsfsdfsf

--- bitrix/.settings.php
+++ bitrix/.settings.php
@@ -27,7 +27,7 @@
   array (
     'value' => 
     array (
-      'debug' => false,
+      'debug' => true,
       'handled_errors_types' => 4437,
       'exception_errors_types' => 4437,
       'ignore_silence' => false,
--- bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
+++ bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001485109583';
-$dateexpire = '001485113183';
+$datecreate = '001485134365';
+$dateexpire = '001485137965';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:1;}';
 return true;
 ?>
