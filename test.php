465385d140d86bc6d36bc427cd7aa3f587a413ed
sim.i.a <simakovich77@gmail.com>
2017-01-27 16:56:43 +0300

    dfgdfgdfg

--- test.php
+++ test.php
@@ -2,13 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("");
 
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form",
-	"",
-	Array(
-		"FORGOT_PASSWORD_URL" => "/user/",
-		"PROFILE_URL" => "/user/profile.php",
-		"REGISTER_URL" => "/user/registration.php",
-		"SHOW_ERRORS" => "N"
-	)
+?><?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
+	"FORGOT_PASSWORD_URL" => "/user/",	// Страница забытого пароля
+		"PROFILE_URL" => "/user/profile.php",	// Страница профиля
+		"REGISTER_URL" => "/user/registration.php",	// Страница регистрации
+		"SHOW_ERRORS" => "N",	// Показывать ошибки
+	),
+	false
 );?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
