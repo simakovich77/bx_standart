d7dda90b51e580ca26148bbf75fc89c9b2223eca
sim.i.a <simakovich77@gmail.com>
2017-01-31 14:53:43 +0300

    fgfgffgfg

--- bitrix/templates/.default/components/bitrix/system.auth.form/auth/template.php
+++ bitrix/templates/.default/components/bitrix/system.auth.form/auth/template.php
@@ -1,25 +1,10 @@
+
+
+
+
+
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<? dump($arResult, false, true) ?>
 
-<span class="hd_singin"><a id="hd_singin_but_open" href="">Войти на сайт</a>
-							<div class="hd_loginform">
-								<span class="hd_title_loginform">Войти на сайт</span>
-								<form name="" method="" action="">
-
-									<input placeholder="Логин"  type="text">
-									<input  placeholder="Пароль"  type="password">
-									<a href="/" class="hd_forgotpassword">Забыли пароль</a>
-
-									<div class="head_remember_me" style="margin-top: 10px">
-										<input id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" type="checkbox">
-										<label for="USER_REMEMBER_frm" title="Запомнить меня на этом компьютере">Запомнить меня</label>
-									</div>
-									<input value="Войти" name="Login" style="margin-top: 20px;" type="submit">
-									</form>
-								<span class="hd_close_loginform">Закрыть</span>
-							</div>
-							</span><br>
-<a href="" class="hd_signup">Зарегистрироваться</a>
 
 
 
@@ -44,11 +29,23 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
 	<input type="hidden" name="AUTH_FORM" value="Y" />
 	<input type="hidden" name="TYPE" value="AUTH" />
 
-<input placeholder="<?=GetMessage("AUTH_LOGIN")?>"  name="USER_LOGIN" value="<?=$arResult["USER_LOGIN"]?>" type="text">
+<input placeholder="<?GetMessage("AUTH_LOGIN")?>"  name="USER_LOGIN" value="<?=$arResult["USER_LOGIN"]?>" type="text">
 <input  placeholder="<?=GetMessage("AUTH_PASSWORD")?>"  name="USER_PASSWORD" type="password">
 
 
-<a href="/" class="hd_forgotpassword">Забыли пароль</a>
+
+
+<noindex><a href="<?=$arResult["AUTH_FORGOT_PASSWORD_URL"]?>" class="hd_forgotpassword" rel="nofollow"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a></noindex>
+
+
+
+
+
+
+
+
+
+
 
 	<?if ($arResult["STORE_PASSWORD"] == "Y"):?>
 
@@ -62,6 +59,17 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 <?if ($arResult["CAPTCHA_CODE"]):?>
 
 			<?echo GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
@@ -70,90 +78,24 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
 			<input type="text" name="captcha_word" maxlength="50" value="" /></td>
 
 <?endif?>
-		<tr>
-			<td colspan="2"><input type="submit" name="Login" value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>" /></td>
-		</tr>
-<?if($arResult["NEW_USER_REGISTRATION"] == "Y"):?>
-		<tr>
-			<td colspan="2"><noindex><a href="<?=$arResult["AUTH_REGISTER_URL"]?>" rel="nofollow"><?=GetMessage("AUTH_REGISTER")?></a></noindex><br /></td>
-		</tr>
-<?endif?>
-
-		<tr>
-			<td colspan="2"><noindex><a href="<?=$arResult["AUTH_FORGOT_PASSWORD_URL"]?>" rel="nofollow"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a></noindex></td>
-		</tr>
-<?if($arResult["AUTH_SERVICES"]):?>
-		<tr>
-			<td colspan="2">
-				<div class="bx-auth-lbl"><?=GetMessage("socserv_as_user_form")?></div>
-<?
-$APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "icons", 
-	array(
-		"AUTH_SERVICES"=>$arResult["AUTH_SERVICES"],
-		"SUFFIX"=>"form",
-	), 
-	$component, 
-	array("HIDE_ICONS"=>"Y")
-);
-?>
-			</td>
-		</tr>
-<?endif?>
-	</table>
-</form>
-
-<?if($arResult["AUTH_SERVICES"]):?>
-<?
-$APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "", 
-	array(
-		"AUTH_SERVICES"=>$arResult["AUTH_SERVICES"],
-		"AUTH_URL"=>$arResult["AUTH_URL"],
-		"POST"=>$arResult["POST"],
-		"POPUP"=>"Y",
-		"SUFFIX"=>"form",
-	), 
-	$component, 
-	array("HIDE_ICONS"=>"Y")
-);
-?>
-<?endif?>
-
-<?
-elseif($arResult["FORM_TYPE"] == "otp"):
-?>
-
-<form name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
-<?if($arResult["BACKURL"] <> ''):?>
-	<input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" />
-<?endif?>
-	<input type="hidden" name="AUTH_FORM" value="Y" />
-	<input type="hidden" name="TYPE" value="OTP" />
-	<table width="95%">
-		<tr>
-			<td colspan="2">
-			<?echo GetMessage("auth_form_comp_otp")?><br />
-			<input type="text" name="USER_OTP" maxlength="50" value="" size="17" autocomplete="off" /></td>
-		</tr>
-<?if ($arResult["CAPTCHA_CODE"]):?>
-		<tr>
-			<td colspan="2">
-			<?echo GetMessage("AUTH_CAPTCHA_PROMT")?>:<br />
-			<input type="hidden" name="captcha_sid" value="<?echo $arResult["CAPTCHA_CODE"]?>" />
-			<img src="/bitrix/tools/captcha.php?captcha_sid=<?echo $arResult["CAPTCHA_CODE"]?>" width="180" height="40" alt="CAPTCHA" /><br /><br />
-			<input type="text" name="captcha_word" maxlength="50" value="" /></td>
-		</tr>
-<?endif?>
-<?if ($arResult["REMEMBER_OTP"] == "Y"):?>
-		<tr>
-			<td valign="top"><input type="checkbox" id="OTP_REMEMBER_frm" name="OTP_REMEMBER" value="Y" /></td>
-			<td width="100%"><label for="OTP_REMEMBER_frm" title="<?echo GetMessage("auth_form_comp_otp_remember_title")?>"><?echo GetMessage("auth_form_comp_otp_remember")?></label></td>
-		</tr>
-<?endif?>
-
 
 		<input value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>" name="Login" style="margin-top: 20px;" type="submit">
 
-		
+
+		</form>
+	<span class="hd_close_loginform">Закрыть</span>
+	</div>
+	</span><br>
+
+<?if($arResult["NEW_USER_REGISTRATION"] == "Y"):?>
+
+<noindex><a href="<?=$arResult["AUTH_REGISTER_URL"]?>" class="hd_signup" rel="nofollow"><?=GetMessage("AUTH_REGISTER")?></a></noindex>
+
+<?endif?>
+
+
+
+
 		<tr>
 			<td colspan="2"><noindex><a href="<?=$arResult["AUTH_LOGIN_URL"]?>" rel="nofollow"><?echo GetMessage("auth_form_comp_auth")?></a></noindex><br /></td>
 		</tr>
@@ -187,4 +129,4 @@ else:
 	</table>
 </form>
 <?endif?>
-</div>
+
