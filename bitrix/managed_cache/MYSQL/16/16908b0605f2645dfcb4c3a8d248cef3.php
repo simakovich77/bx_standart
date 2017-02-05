e7491fa7a360b891bf1fbbc2a5c247866b3f5291
simakovich77 <simakovich77@gmail.com>
2017-02-05 04:32:03 +0300

    sdfsdfsdfsdfsF111

--- bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
+++ bitrix/managed_cache/MYSQL/16/16908b0605f2645dfcb4c3a8d248cef3.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001486239843';
-$dateexpire = '001486243443';
+$datecreate = '001486255306';
+$dateexpire = '001486258906';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";b:1;}';
 return true;
 ?>
