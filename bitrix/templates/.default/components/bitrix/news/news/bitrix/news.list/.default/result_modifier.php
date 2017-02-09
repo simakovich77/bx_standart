f1082a7ce06da3aee8dd16202753848b0a3bccde
simakovich77 <simakovich77@gmail.com>
2017-02-09 04:07:36 +0300

    fdfdfdf

--- bitrix/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
+++ bitrix/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
@@ -1,12 +1,13 @@
 
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
-<? dump($arParams)?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
-<? foreach ($arResult["ITEMS"] as $ID=>$arItems)
+foreach ($arResult["ITEMS"] as $ID=>$arItems)
 {
 $arImage=CFile::ResizeImageGet($arItems['DETAIL_PICTURE'], array('width'=>$arParams['LIST_PREV_PICT_W'], 'height'=>$arParams['LIST_PREV_PICT_H']), BX_RESIZE_IMAGE_PROPORTIONAL, true);
 
-    $arResult["ITEMS"][];
+    $arResult["ITEMS"][$ID]['DETAIL_PICTURE']=$arImage;
 }
+
+dump($arResult["ITEMS"]);
 
     ?>
